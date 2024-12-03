1e00babd756af04386391661671123ab60375710
facuperezm <facundoperezmontalvo@gmail.com>
2024-12-03 12:12:22 -0300

    feat: add Booking and Service interfaces to enhance type safety and structure for booking management

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,3 +6,24 @@ export interface Barber {
   imageUrl: string | null;
   createdAt: Date | null;
 }
+
+export interface Booking {
+  id: number;
+  customerName: string;
+  service: string;
+  barber: string;
+  date: string;
+  time: string;
+  status: "pending" | "confirmed" | "cancelled" | "completed";
+  customerEmail: string;
+  customerPhone: string;
+}
+
+export interface Service {
+  description: string | null;
+  id: number;
+  name: string;
+  createdAt: Date | null;
+  price: number;
+  duration: number;
+}
